cbb6f092446e5e21181a5e4c3613c5ab4dc54b6d
viswanathamsantosh <viswanathamsantosh@gmail.com>
2019-04-22 22:50:15 +0530

    removed CE references

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,5 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
-  serviceIp: 'https://staging.cloud-elements.com/elements/api-v2',
-  // tslint:disable-next-line:max-line-length
-  apiToken: 'User x6/5Bq/w95a/LcuWRdTM/zLrUOW62du7JP5Fak0xIkk=, Organization a854a32b6bcb5fb9a9aa723a0379eb83, Element YCNbNjeuloqjEd5isYM27/uNl7pOQkXa3XsFJgqvd54=',
-  folderId: '1Frk2DzAq_MKkzyULO4HxMwXkza7dMrW0'
+  production: false
 };
